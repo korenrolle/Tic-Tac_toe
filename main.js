2258bd7523946350eff5c1e56bf061903555d469
korenrolle <korenrolle21@gmail.com>
2022-11-11 11:02:12 -0800

    Winning array

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
-const rows = document.querySelectorAll('.box');
-rowArr = [];
-rows.forEach((row) => {
-  rowArr.push(row);
-  console.log(rowArr);
-});
-function startGame() {
-  let userChoice = prompt(
-    'Would you like to continue the long lasting battle for the Alphabet Crown?',
-    'Y/N'
-  );
-  if (userChoice === 'Y') {
-    playGame();
-  }
-}
-startGame();
+// const rows = document.querySelectorAll('.box');
+// rowArr = [];
+// rows.forEach((row) => {
+//   rowArr.push(row);
+//   console.log(rowArr);
+// });
+// function startGame() {
+//   let userChoice = prompt(
+//     'Would you like to continue the long lasting battle for the Alphabet Crown?',
+//     'Y/N'
+//   );
+//   if (userChoice === 'Y') {
+//     playGame();
+//   }
+// }
 
-function startGame() {}
-// function colorChange(emptySpace) {
+let player = 1;
+
+let currentChoice = [player1, player2];
+console.log(player1);
+// startGame();
+if
+function playGame() {
+  let player1 = ['1,3,5,7,9']
+  let player2 = ['2,4,6,8']
+  let setInterval(() => {
+    
+  }, interval);
+
+  }
+  // /if
+  
+  // for (let i = 1; i === rows.length; i++)
+  //   console.log(' Sorry bro..Already occupied');
+// }
+// // function colorChange(emptySpace) {
 // for (let i = 1; i === rows.length; i++)
 //   //   console.log(' Sorry bro..Already occupied');
 //   document
@@ -34,3 +51,13 @@ function startGame() {}
 // Fuctions for button
 //Player 1/ Player 2 Array
 //Create Alert(Prompt) function that is triggered 5 sec after the page loads
+const findWinner = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+];
