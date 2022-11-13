921a9126de1749e1816badf8ae4a654147c25897
korenrolle <korenrolle21@gmail.com>
2022-11-13 04:58:21 -0800

    Got the willing array to compare to the winner1,winner2

--- main.js
+++ main.js
@@ -15,102 +15,143 @@ const findWinner = [
   [0, 4, 8],
   [6, 4, 2]
 ];
+// const compareFn = (winner1, findWinner) => {
+//   return JSON.stringify(winner1) === JSON.stringify(findWinner);
+// };
+// console.log(compareFn(winner2, findWinner));
+
 rowsArr.map((row, index) => {
   row.addEventListener('click', function ticTac(test) {
-    console.log();
     if (currentChoice == 1) {
+      row.className = 'noClick1';
       row.innerHTML = player1;
-      row.className = 'noClick';
-      // compareFn();
+
+      win();
       currentChoice = 2;
       winner1.push(index);
       console.log(winner1);
     } else if (currentChoice == 2) {
+      row.className = 'noClick2';
       row.innerHTML = player2;
-      row.className = 'noClick';
-      // compareFn();
+      win();
       currentChoice = 1;
       winner2.push(index);
       console.log(winner2);
     }
   });
 });
-for (let i = 0; i < findWinner.length; i++) {
-  console.log(findWinner[i]);
-  // function compareFn(winner1, winner2) {
-  if (winner1 === findWinner[i]) {
-    console.log('You win player 1');
-    // rows.className = 'noClick';
-  } else if (winner2 === findWinner[i]) {
-    console.log('you win player 2');
-    // rows.className = 'noClick';
-    // }
+// let win = findWinner[i]
+function win() {
+  for (let i = 0; i < findWinner.length; i++) {
+    let winner = findWinner[i];
+    if (winner.every((win) => rowsArr[win].classList.contains('noClick1'))) {
+      console.log('Player1');
+    } else if (
+      winner.every((win) => rowsArr[win].classList.contains('noClick2'))
+    ) {
+      console.log('Player2');
+    }
   }
 }
 
-// }else {
-//   element.className = "myStyle";
+// if((...win)==(...winner1)){console.log("Finally")}
+// function compareFn(winner1, winner2) {
+// if () {
+//   console.log('You win player 1');
+//   // rows.className = 'noClick';
+// } else if (JSON.stringify(winner2) == JSON.stringify(findWinner[i])) {
+//   console.log('you win player 2');
+//   // rows.className = 'noClick';
 // }
-// rowsObj.addEventListener('click' test))
-// document.getElementBy('').addEventListener('click', function (test) {
-//   console.log('This works');
-// });
-// document.querySelectorAll(rows).addEventListener('click',
-
-// row.onClick = function ( {
-// test()
-// })
-// addEventListener;
-// console.log('This works');
-
-// function startGame() {
-//   let userChoice = prompt(
-//     'Would you like to continue the long lasting battle for the Alphabet Crown?',
-//     'Y/N'
-//   );
-//   if (userChoice === 'Y') {
-//     playGame();
-//   }
 // }
+// //   const compareFn = (winner1, findWinner) => {
+// //     return winner1.toString() === findWinner[i].toString();
+// //   };
+// // });
 
-// let player = 1;
+// //     for (var i = 0; i < a.length; i++) {
+// //       if (a[i] !== b[i]) {
+// //         return false;
+// //       }
+// //     }
+// //     return true;
+// //   }
+// // };
 
-// let currentChoice = [player1, player2];
-// console.log(player1);
-// // startGame();
-// if
-// function playGame() {
-//   let player1 = [1, 3, 5, 7, 9]
-//   let player2 = [2,4,6,8]
+// // }else {
+// //   element.className = "myStyle";
+// // }
+// // rowsObj.addEventListener('click' test))
+// // document.getElementBy('').addEventListener('click', function (test) {
+// //   console.log('This works');
+// // });
+// // document.querySelectorAll(rows).addEventListener('click',
 
-//   }
-//   // /if
+// // row.onClick = function ( {
+// // test()
+// // })
+// // addEventListener;
+// // console.log('This works');
 
-// for (let i = 1; i === rows.length; i++)
-//   console.log(' Sorry bro..Already occupied');
+// // function startGame() {
+// //   let userChoice = prompt(
+// //     'Would you like to continue the long lasting battle for the Alphabet Crown?',
+// //     'Y/N'
+// //   );
+// //   if (userChoice === 'Y') {
+// //     playGame();
+// //   }
+// // }
+
+// // let player = 1;
+
+// // let currentChoice = [player1, player2];
+// // console.log(player1);
+// // // startGame();
+// // if
+// // function playGame() {
+
+// //   }
+// //   // /if
+
+// // for (let i = 1; i === rows.length; i++)
+// //   console.log(' Sorry bro..Already occupied');
+// // }
+// // // function colorChange(emptySpace) {
+// if (
+//   findWinner[i][0] == winner1[0] &&
+//   findWinner[i][1] == winner1[1] &&
+//   findWinner[i][2] == winner1[2]
+// ) {
+//   console.log('finally');
+// } else if (
+//   findWinner[i][0] == winner2[0] &&
+//   findWinner[i][1] == winner2[1] &&
+//   findWinner[i][2] == winner2[2]
+// ) {
+//   console.log('finally 2');
 // }
-// // function colorChange(emptySpace) {
-// for (let i = 1; i === rows.length; i++)
-//   //   console.log(' Sorry bro..Already occupied');
-//   document
-//   .getElementsByClassName('board')
-//   .addEventListener('click', colorChange);
-// }
-// console.log(colorChange);
+// // for (let i = 1; i === rows.length; i++)
+// //   //   console.log(' Sorry bro..Already occupied');
+// //   document
+// //   .getElementsByClassName('board')
+// //   .addEventListener('click', colorChange);
+// // }
+// // console.log(colorChange);
 
-// function gameBoard(row`
-// }
-//Functions
-// A comparison/winning function
-// A function that changes the color (on click)
-//A reset function
-// Fuctions for button
-//Player 1/ Player 2 Array
-//Create Alert(Prompt) function that is triggered 5 sec after the page loads
+// // function gameBoard(row`
+// // }
+// //Functions
+// // A comparison/winning function
+// // A function that changes the color (on click)
+// //A reset function
+// // Fuctions for button
+// //Player 1/ Player 2 Array
+// //Create Alert(Prompt) function that is triggered 5 sec after the page loads
 
-// console.log(findWinner[1]);
-// if (findWinner[0]==winner1 || ){
+// // console.log(findWinner[1]);
+// // if (findWinner[0]==winner1 || ){
 
-// }
+// // }
 
-// compareFn();
+// // compareFn();
