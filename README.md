bd332564cd2f39c156636caefe788739a95afb96
korenrolle <korenrolle21@gmail.com>
2023-01-23 21:41:51 -0800

    minor changes

--- README.md
+++ README.md
@@ -2,15 +2,18 @@ PSEUDO CODE
 
 1. Create a 3x3 array to represent the game board
 2. Create a function to check for a win
-  - Check all rows, if any row has the same value return that value as the winner
-  - Check all columns, if any column has the same value return that value as the winner
-  - Check the two diagonals, if any diagonal has the same value return that value as the winner
-  - If no winner is found, check if the board is full, if so return "Tie"
+
+- Check all rows, if any row has the same value return that value as the winner
+- Check all columns, if any column has the same value return that value as the winner
+- Check the two diagonals, if any diagonal has the same value return that value as the winner
+- If no winner is found, check if the board is full, if so return "Tie"
+
 3. Create a function to make a move
-  - Validate that the chosen position is empty and within the bounds of the board
-  - If valid, update the board with the player's symbol
-  - Check for a win, if a winner is found return the winner's symbol
-  - If no winner is found, check if the board is full, if so return "Tie"
+
+- Validate that the chosen position is empty and within the bounds of the board
+- If valid, update the board with the player's symbol
+- Check for a win, if a winner is found return the winner's symbol
+- If no winner is found, check if the board is full, if so return "Tie"
 
 # USERSTORIES
 
@@ -28,6 +31,3 @@ As a player, I want to be able to save my game progress so that I can resume the
 WIREFRAME
 
 ![tictactoe](https://user-images.githubusercontent.com/115126618/213927727-fb9bc7a0-6614-4b40-a318-629956d9dea0.png)
-
-
-
--- main.js
+++ main.js
@@ -16,56 +16,43 @@ const findWinner = [
   [0, 4, 8],
   [6, 4, 2]
 ];
-
-// this is where I mapped to the specific div the will hold the X,O as well as my if statement. Once the player click it stores the index of the specific div they click on in an array that stores it for comparison between the findWinner.
-//my if statement is saying that if currentChoice which is the respective players turn then switch the class to a noClick class to stop them from being able to click and target the inner HTML for the letter. After that, run the win fucntion to see if anything matches and then if not, switch back to currentChoice1 which controls the turns.
-// Once the pl
-
-rowsArr.map((row, index) => {
-  row.addEventListener('click', function ticTac(test) {
-    if (currentChoice == 1) {
-      row.className = 'noClick1';
-      row.innerHTML = player1;
-      win();
-      currentChoice = 2;
-      winner1.push(index);
-      console.log(winner1);
-    } else if (currentChoice == 2) {
-      row.className = 'noClick2';
-      row.innerHTML = player2;
-      win();
-      currentChoice = 1;
-      winner2.push(index);
-      console.log(winner2);
-    }
-  });
+rowsArr.forEach((row, index) => {
+  row.addEventListener('click', ticTac);
 });
-// This is my endGame function. This says that once we find a winner switch the class to the endgame class. I targeted my class specifically  with DOM.
-function endGame1() {
-  document.getElementById('endGame').className = 'endGame1';
-  console.log('game end 1');
+function ticTac(e) {
+  if (currentChoice === 1) {
+    e.target.className = 'noClick1';
+    e.target.innerHTML = player1;
+    win();
+    currentChoice = 2;
+    winner1.push(rowsArr.indexOf(e.target));
+  } else if (currentChoice === 2) {
+    e.target.className = 'noClick2';
+    e.target.innerHTML = player2;
+    win();
+    currentChoice = 1;
+    winner2.push(rowsArr.indexOf(e.target));
+  }
 }
-// This is my win function. This was VERY VERY HARD lol
-// This says that if i is less that findWinner.length then keep adding numbers
-//
 function win() {
   for (let i = 0; i < findWinner.length; i++) {
     let winner = findWinner[i];
     if (winner.every((win) => rowsArr[win].classList.contains('noClick1'))) {
-      console.log('player 1 wins');
       winnerDisplay.innerHTML = 'Player 1 Wins';
       endGame1();
     } else if (
       winner.every((win) => rowsArr[win].classList.contains('noClick2'))
     ) {
-      console.log('player 2 wins');
       winnerDisplay.innerHTML = 'Player 2 Wins';
       endGame1();
-    } else if (winner1.length === 4 || winner2.length === 4) {
+    } else if (winner1.length + winner2.length === 9) {
       winnerDisplay.innerHTML = 'Its a draw';
       endGame1();
     }
   }
+}
+function endGame1() {
+  document.getElementById('endGame').classList.add('endGame1');
 }
 
 function refresh() {
